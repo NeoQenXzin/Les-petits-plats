ba387bab3bc5f5c3cde2c96391b1f29b9968f3f8
Dragonk1 <keyntao99@yahoo.fr>
2022-06-02 14:40:56 +0200

    sauvegarde main avant creation branch

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -6,11 +6,17 @@ class App {
       // this.recipesApi = new RecipesApi('https://neoqenxzin.github.io/Les-petits-plats/data/recipes.json')
     }
   
+    async getAllRecipesData() {
+    const recipesData = await this.recipesApi.getAllRecipes() 
+    return recipesData
+  }
+    
     async displayRecipes () {
       // Je récupère mes datas json
-      const recipesData = await this.recipesApi.getAllRecipes()     
+      const recipesData = await this.getAllRecipesData() 
       // Je prepare l'accès de mes templates a ma RecipeFactory
       const RecetteFactory = new RecipeFactory()
+      
       recipesData.forEach(recipe => {
         const Recette = RecetteFactory.createRecipe(recipe)
         // console.log(Recette, Recette.ingredients);
